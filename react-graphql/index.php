f54daaf160bb6152c1de33e11cfe00df7b4d3fe2
Matt Poulter <admin.matt.poulter@racingpost.local>
2020-12-20 22:49:59 +0000

    components working with individual graphql queries

--- react-graphql/index.php
+++ react-graphql/index.php
@@ -2,10 +2,10 @@
 
 get_header();
 
-$posts = get_posts();
+$posts = get_posts(['fields' => 'ids']);
 
-foreach($posts as $post): ?>
-    <div data-react="Article" data-props='<?= json_encode($post); ?>'></div>
+foreach($posts as $post_id): ?>
+    <div data-react="Article" data-props='<?= json_encode(['id' => $post_id]); ?>'></div>
 <? endforeach; 
 
 get_footer();
--- react-graphql/src/Article.jsx
+++ react-graphql/src/Article.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Article.scss';
 
-const Article = ({post_title}) => {
+const Article = ({id}) => {
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                query: `
+                    {
+                        postBy(postId: ${id}) {
+                        title
+                        myfieldgroup {
+                            subtitle
+                        }
+                        }
+                    }
+                  
+                `,
+            }),
+            })
+            .then(res => res.json())
+            .then(res => setPost(res.data.postBy))
+    }, []);
+
+    if(!post) return null;
+
+    const { title, myfieldgroup: { subtitle } } = post; 
+
     return (
-        <div>
-            {post_title}
+        <div className="Article">
+            {title}
+            <span>{subtitle}</span>
         </div>
     )
 }
